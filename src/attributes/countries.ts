9f47701bb272cda64c07f8cd0899a68e97278b95
Weston Siegenthaler <westons+github@gmail.com>
2022-11-09 19:47:34 -0800

    added warning to result object for unknown country codes (shouldn't ever occur since the parser only accepts valid ones, but this may change later)

--- src/attributes/countries.ts
+++ src/attributes/countries.ts
@@ -1,6 +1,14 @@
-import { Field } from './builders'
+import { Field, ValueDef } from './builders'
 import { UICCountryCodeMap } from '../uic/countries'
+import { ParseWarnings } from './vehicles/common-fields'
 
 const CountryField = new Field<object>('Country', 'country')
 
-export const CountryByCode = (code: number) => CountryField.value(UICCountryCodeMap[code])
+export const CountryByCode = (code: number): ValueDef<any> => {
+  const def = UICCountryCodeMap[code]
+  if (def === undefined) {
+    return ParseWarnings.value(`Country code '${code}' doesn't appear to be a known value.`)
+  } else {
+    return CountryField.value(UICCountryCodeMap[code])
+  }
+}
