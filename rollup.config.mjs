b4f8170b43c96b86ea6e62933d109dc1fad6c0d6
Weston Siegenthaler <westons+github@gmail.com>
2022-11-10 14:32:51 -0800

    omit umd build when in watch mode

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,49 +8,54 @@ import analyze from 'rollup-plugin-analyzer'
 import pkg from './package.json' assert { type: "json" }
 
 
-export default [
-  // browser & node friendly UMD build
-  {
-    input: 'src/index.ts',
-    output: {
-      name: 'railID',
-      file: pkg.browser,
-      format: 'umd'
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript(),
-      babel({
-        presets: ["@babel/preset-env"],
-        babelHelpers: 'bundled',
-        targets: "defaults or cover 96%"
-      }),
-      terser(),
-      analyze({ limit: 20, summaryOnly: true })
-    ]
+// browser & node friendly UMD build
+const umdConfig = {
+  input: 'src/index.ts',
+  output: {
+    name: 'railID',
+    file: pkg.browser,
+    format: 'umd'
   },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    babel({
+      presets: ["@babel/preset-env"],
+      babelHelpers: 'bundled',
+      targets: "defaults or cover 96%"
+    }),
+    terser(),
+    analyze({ limit: 20, summaryOnly: true })
+  ]
+}
 
-  // CommonJS (for Node) and ES module (for bundlers) build.  (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify `file` and `format` for each target)
-  {
-    input: 'src/index.ts',
-    external: [],
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
-    ],
-    plugins: [
-      typescript(),
-      babel({
-        presets: ["@babel/preset-env"],
-        babelHelpers: 'bundled',
-        targets: [
-          "maintained node versions",
-          "last 3 versions"
-        ]
-      })
-    ]
-  }
-]
+// CommonJS (for Node) and ES module (for bundlers) build.  (We could have three entries in the configuration array
+// instead of two, but it's quicker to generate multiple builds from a single configuration where possible, using
+// an array for the `output` option, where we can specify `file` and `format` for each target)
+const esCjsConfig = {
+  input: 'src/index.ts',
+  external: [],
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
+  ],
+  plugins: [
+    typescript(),
+    babel({
+      presets: ["@babel/preset-env"],
+      babelHelpers: 'bundled',
+      targets: [
+        "maintained node versions",
+        "last 3 versions"
+      ]
+    })
+  ]
+}
+
+export default cliArgs => {
+  // Only produce UMD target if not in watch mode
+  return cliArgs.watch ?  [ esCjsConfig ] : [ esCjsConfig, umdConfig ]
+}
+
+
