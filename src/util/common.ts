c9480f5f28a8ed0e8514a7ffb7f71d11e7426b84
Weston Siegenthaler <westons@gmail.com>
2024-02-29 19:38:34 -0800

    code cleaning now also strips zero-width spaces. minor bump

--- src/util/common.ts
+++ src/util/common.ts
@@ -10,10 +10,20 @@ export interface Dictionary<T> {
 const removeCombiningDiacritics = (s: string) =>
   s.split('').filter(c => c.charCodeAt(0) < 768 || c.charCodeAt(0) > 879).join('') 
 
+// Removes miscellaneous unwanted characters
+const removeZeroWidthSpaces = (s: string) =>
+  s.split('').filter(c => c.charCodeAt(0) !== 8203).join('') 
+
 // Reddies input code for parsing by:
 // 1) Removing redundant whitespace
 // 2) Replacing tabs with spaces
 // 3) Removing diacritic unicode characters, include 'combining diacritics'
+// 4) Removing zero-width spaces
 //
 // All but (3) are unessential for parsing but provide cleaner display/sourcemaps
-export const cleanRawInput = (rawInput: string) => removeCombiningDiacritics(removeDiacritics(rawInput.replaceAll(/\s+/g, ' ')))
+export const cleanRawInput = (rawInput: string) => [ rawInput.replaceAll(/\s+/g, ' ') ]
+  .map(removeCombiningDiacritics)
+  .map(removeDiacritics)
+  .map(removeZeroWidthSpaces)
+  .pop()
+
